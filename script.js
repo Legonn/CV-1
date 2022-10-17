e5a8ade5a9c4a81f9daa5286a928d3b5d2d0c2cc
Alexandru Botez <88148599+Legonn@users.noreply.github.com>
2022-10-17 21:15:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function afiseazaText() {
+  document.querySelector("#experienta1").classList.toggle("element-ascuns");
+  //document.querySelector -> ne returneaza elementul
+  //classList -> ne ofere o lista cu clasele
+  //toggle -> va adauga o clasa in cazul in care ea nu este in lista, si o va sterege daca este in lista ( se aplica doar pentru class list)
+}
+function afiseazaText2() {
+  document.querySelector("#experienta2").classList.toggle("element-ascuns");
+}
+function afiseazaText3() {
+  document.querySelector("#experienta3").classList.toggle("element-ascuns");
+}
